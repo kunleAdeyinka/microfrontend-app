cd6f11c9b62032db904c3b6a99c4e9231277cb94
Kunle Adeyinka <aadeyink@gmail.com>
2021-04-21 05:04:05 -0400

    fixed aws-cli build error by editing container/App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -6,7 +6,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div>
-            <h1>Hello container </h1>
+            <h1>Hello container app is working </h1>
             <hr/>
             <MarketingApp />
         </div>
